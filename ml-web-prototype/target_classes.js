15572582e532b16a31a2fee247bed7f7b1fb82e5
ryan_nolly <rananda61@gmail.com>
2021-06-09 00:04:40 +0700

    Adding a finished web prototype for the ML

---
+++ ml-web-prototype/target_classes.js
@@ -0,0 +1,11 @@
+TARGET_CLASSES = {
+  0: "Bubur Ayam",
+  1: "Hot Dog",
+  2: "Mie Goreng",
+  3: "Nasi Goreng",
+  4: "Nasi Putih",
+  5: "Rendang",
+  6: "Sushi",
+  7: "Telur Ceplok",
+  8: "Telur Dadar"
+};
